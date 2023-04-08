806320b7d2bca74c61dc2ffc483eeb4ecca2d8f2
ayoakin <ayoakinkugbe@outlook.com>
2023-04-08 19:00:35 -0400

    models commit

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'littlelemon',
+        'NAME': 'LittleLemon',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
