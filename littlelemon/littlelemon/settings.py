cf634c10566088e3f19ecba62fa606d67c8d8c1c
ayoakin <ayoakinkugbe@outlook.com>
2023-04-12 22:53:38 -0400

    Auth Commit

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "restaurant",
     "rest_framework",
+    "rest_framework.authtoken",
     
 ]
 
@@ -125,3 +126,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- littlelemon/restaurant/url.py
+++ littlelemon/restaurant/url.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('table/', views.TableItemView.as_view()),
     path('table/<int:pk>/', views.SingleTableItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token)
     ]
